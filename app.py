5dbbabd81beaa0e944e9bd00fe85438d5b3dd5e4
artur <yo13u@example.com>
2026-08-24 11:58:52 +0300

    updated app

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
 
 app.run(debug=True)
