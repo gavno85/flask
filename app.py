9267640069f73ab0d57edabeb3d936fbec5337fc
artur <yo13u@example.com>
2026-09-02 16:53:08 +0300

    edited books.html

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from book_list import books
 
 app = Flask(__name__)
+
+@app.route("/")
+def book():
+    return render_template("books.html", book_list= books)
 
 app.run(debug=True)
