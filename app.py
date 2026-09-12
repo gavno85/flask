8ba3e68a1f96c1b991de3ea5ad64a8be8fd52ba0
artur <yo13u@example.com>
2026-09-12 18:43:18 +0300

    asd

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/delete/<int:index_wish>')
+def delete(index_wish):
+    flash(f'wish {all_wishes[index_wish]} deleted')
+    all_wishes.pop(index_wish)
+    return redirect(url_for('main'))
+
 app.run(debug=True)
