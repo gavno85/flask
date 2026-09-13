71fbae61acfce7198197eee4f61f0367edabdec7
artur <yo13u@example.com>
2026-09-13 15:02:12 +0300

    edited main and register and added admin

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = "asdad"
+all_register= []
 
 @app.route('/')
 def main():
@@ -12,12 +13,17 @@ def register():
     if request.method == "POST":
         username = request.form.get("username")
         user_phone = request.form.get('phone')
-        if len(user_phone) != 12 or user_phone[:4] != "+380":
+        if len(user_phone) != 13 or user_phone[:4] != "+380":
             flash("input incorrect number")
         else:
+            all_register.append({'username': username, 'user_phone': user_phone})
             flash('you registered')
-            return redirect(url_for('register'))
+            return redirect(url_for('main'))
         
     return render_template('register.html')
 
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', all_register= all_register)
 app.run(debug=True)
