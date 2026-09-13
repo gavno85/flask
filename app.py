7bfef300256a9f1b9af8637e502239438f8ad191
artur <yo13u@example.com>
2026-09-13 14:24:44 +0300

    edited main and added register

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = "asdad"
 
 @app.route('/')
-@app.route('/welcome')
-def welcome():
-    return render_template("welcome.html", name = "Artur")
+def main():
+    return render_template("main.html")
 
+@app.route('/register', methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        user_phone = request.form.get('phone')
+        if len(user_phone) != 12 or user_phone[:4] != "+380":
+            flash("input incorrect number")
+        else:
+            flash('you registered')
+            return redirect(url_for('register'))
+        
+    return render_template('register.html')
 
 app.run(debug=True)
