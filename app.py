67bf85155961758f4b1c75bf1f8d79ab30c2db97
artur <yo13u@example.com>
2026-09-16 21:30:43 +0300

    was edited all html files and app py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from os import name
+
 from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
@@ -25,7 +27,31 @@ def products():
 
     return render_template('products.html', all_products=all_products)
 
-@app.route('/delete/<int: key_product>')
-def delete(key_product):
-    all_products.pop(key_product)
+@app.route('/delete_product/<key_product>')
+def delete_product(key_product):
+    if key_product in all_products.keys():
+        deleted_product = all_products.pop(key_product)
+        flash(f'product {deleted_product} deleted')
+    return redirect(url_for('products'))
+
+@app.route('/edit/<name_product>', methods= ["GET", "POST"])
+def edit_product(name_product):
+    product = all_products[name_product]
+    if product:
+        old_category = all_products.get(name_product)['category']
+        old_price = all_products.get(name_product)['price']
+    else:
+        old_category = 'not finded'
+        old_price = 'not finded'
+
+    if request.method == "POST":
+        if name_product in all_products.keys():
+            new_category = request.form.get('new_category')
+            new_price = request.form.get('new_price')
+            product = {'category': new_category, 'price': new_price}
+            flash('product was edited')
+            return redirect(url_for('products'))
+        else:
+            flash('product was not finded')
+    return render_template('edit.html', old_category= old_category, old_price= old_price)
 app.run(debug=True)
