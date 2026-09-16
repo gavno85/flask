8f23f3c4c9acd686678c3dcc0b4b078ddf89e0fb
artur <yo13u@example.com>
2026-09-16 17:40:59 +0300

    edited app py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'password'
+all_products = {}
 
-@app.route('/delete/<int:index_wish>')
-def delete(index_wish):
-    flash(f'wish {all_wishes[index_wish]} deleted')
-    all_wishes.pop(index_wish)
-    return redirect(url_for('main'))
+@app.route('/delete/<name_product>')
+def delete():
+    pass
 
+@app.route('/', methods=['GET', "POST"])
+@app.route('/products', methods=["GET","POST"])
+def products():
+    if request.method == "POST":
+        name_product = request.form.get('name_product')
+        price_product = request.form.get('price_product')
+        category_product = request.form.get('category_product')
+
+        if name_product not in all_products.keys():
+            all_products[name_product] = {'category': category_product, 'price': price_product}
+        else:
+            flash(f'product {name_product} is exists')
+
+        return redirect(url_for('products'))
+
+    return render_template('products.html', all_products=all_products)
 app.run(debug=True)
